ec12c5d3709fd2105e8416af7bbff5c7c0d5ce92
149segolte <37300847+149segolte@users.noreply.github.com>
2023-04-20 18:53:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/enigma2048/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.enigma2048;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
